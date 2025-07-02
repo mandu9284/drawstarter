888cf49e6ac0ce76e5983661651b20792c7e9de3
mandu9284 <mandu9284@gmail.com>
2025-07-02 07:48:23 +0000

    feat: 다른 주제 보기 기능 구현

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,21 @@
+"use client";
+
 import { getRandomPrompt } from "@/lib/prompt";
 import Link from "next/link";
+import { useState } from "react";
 
 export default function Home() {
+  const [prompt, setPrompt] = useState(getRandomPrompt());
   return (
     <div className="space-y-6">
     <h1 className="text-3xl font-bold">DrawStarter</h1>
     <div className="border p-4 rounded bg-white shadow">
       <p className="font-semibold">오늘의 주제:</p>
-      <p className="text-lg">{getRandomPrompt()}</p>
+      {/* 서버 컴포넌트에서 렌더링된 내용과 클라이언트 컴포넌트에서 렌더링된 내용이 다르기 때문에 suppressHydrationWarning을 사용 */}
+      <p className="text-lg" suppressHydrationWarning>{prompt}</p>
     </div>
     <div className="flex gap-4">
-      <button className="bg-gray-200 px-4 py-2 rounded">다른 주제 보기</button>
+      <button className="bg-gray-200 px-4 py-2 rounded" onClick={() => setPrompt(getRandomPrompt())}>다른 주제 보기</button>
       <Link href="/draw" className="bg-blue-500 text-white px-4 py-2 rounded">그리기 시작</Link>
     </div>
   </div>
