252d3d04273330854dc7052c018f04c6dc88f6b6
mandu <mandu9284@gmail.com>
2025-07-08 08:39:38 +0900

    fix: lint

--- middleware.ts
+++ middleware.ts
@@ -30,7 +30,5 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: [
-    '/((?!api|_next|public|favicon\\.ico).*)',
-  ],
+  matcher: ['/((?!api|_next|public|favicon\\.ico).*)'],
 }
